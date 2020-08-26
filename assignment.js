1288cb542f7676614da98502c2e13124b16cb776
smellymellyy <69225632+smellymellyy@users.noreply.github.com>
2020-08-26 11:03:52 +1000

    cost base

--- assignment.js
+++ assignment.js
@@ -1,22 +1,49 @@
 window.onload=function() {
-	lengthObj = document.getElementById('txtLength');
-	widthObj = document.getElementById('txtWidth');
-    weightObj = document.getElementById('tdHeight');
+    lengthObj = document.getElementById('txtLength');
+    widthObj = document.getElementById('txtWidth');
+    heightObj = document.getElementById('txtHeight');
+	costObj = document.getElementById('tdCost');
+    document.getElementById('btnCalcCost').onclick = calcCost;
     document.getElementById('btnReset').onclick = resetInputs;
-    document.getElementById('btnCalc').onclick = calcCost;
+
 }
+
 function resetInputs() {
-	lengthObj.value = '';
-	widthObj.value = '';
-    heightObj.innerHTML = '';
+    lengthObj.value = '';
+    widthObj.value = '';
+    heightObj.value = '';
+    costObj.innerHTML = '';
 }
+/* calc cost elements */
+
+
+
+function areaGlass () {
+	var length = new Number(lengthObj.value);
+    var width = new Number(widthObj.value);
+	var height = new Number(heightObj.value);
+	return (width*height*2) + (length*height*2) + (length*width) ;
+
+	/* -Area of glass (width x height x 2) + (length x height x 2) + (length x width)  IF cm2 > 25 x .10    ELSE x .06 */
+}
+
+function glue () {
+	/* -Glue needed (height x 4) + (length x 2) + (width x 2) */
+}
+
+function labour () {
+	/* -Labour   surface area: math sum  if 6000 $60  if 12000 $120   go up in intervals of 6000 */
+}
+
+function gst () {
+	/* GST is (areaGlass) + (glue) + (labour) / 10  
+ subtract from final cost	*/
+} 
+
 function calcCost() {
 	var length = new Number(lengthObj.value);
     var width = new Number(widthObj.value);
 	var height = new Number(heightObj.value);
     costObj.innerHTML = '';
-    if(isNaN(length) || (isNaN(height) || isNaN(width)) {
-		alert('Invalid length, width or height');
-		return;
-	}
-	costObj.innerHTML = length*Math.pow(width,2)/800;
+	costObj.innerHTML = length*;
+}
