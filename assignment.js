1b2a53912241dea632167085f2b4c998ca5eb4bf
smellymellyy <69225632+smellymellyy@users.noreply.github.com>
2020-09-16 11:08:39 +1000

    Update assignment.js

--- assignment.js
+++ assignment.js
@@ -14,19 +14,20 @@ function resetInputs() {
     costObj.innerHTML = '';
 }
 
-function areaGlass () {
+function areaGlass() {
 	var length = new Number(lengthObj.value);
     var width = new Number(widthObj.value);
 	var height = new Number(heightObj.value);
-	return (width*height*2) + (length*height*2) + (length*width);
-}
+		return (width*height*2) + (length*height*2) + (length*width);
+} /* 1.2 */
 
 function glue () {
 	var length = new Number(lengthObj.value);
     var width = new Number(widthObj.value);
 	var height = new Number(heightObj.value);
-	return (height*4) + (length*2) + (width*2);
-}
+	var glueCost = .1;
+		return ((height*4) + (length*2) + (width*2))*glueCost;
+} /* 1.6 */
 
 function labour () {
 	var length = new Number(lengthObj.value);
@@ -35,37 +36,37 @@ function labour () {
 	var surfaceArea = areaGlass ();
 	var labourCost = 60;
 	var labourMath = labourCost/6000;
-	return surfaceArea * labourMath ;
-}
-
-function gst () {
-	var length = new Number(lengthObj.value);
-    var width = new Number(widthObj.value);
-	var height = new Number(heightObj.value);
-	return areaGlass () + glue () + labour ();
-} 
+		return surfaceArea * labourMath ;
+} /* 0.2 */
 
 function calcCost() {
 	var length = new Number(lengthObj.value);
     var width = new Number(widthObj.value);
 	var height = new Number(heightObj.value);
+
+		if (height < 26) {
+		glassCost = .06;
+		} 
+		else {
+		glassCost = .10;
+		}
+
 	var surfaceArea = areaGlass ();
-	var glueCost = glue ();
+	var glassCost;
+	var glass = glassCost*surfaceArea;
+	var glueGlue = glue ();
 	var labourCost = labour ();
-	var tax = gst ();
-	
-	if (height < 26) {
-		surfaceArea *.06;
-	} 
-	else {
-		surfaceArea *.10;
-	}
-	
-	if(isNaN(length) || isNaN(width) || isNaN(height)) {
-                    alert('Invalid length, width or height');
-                    return;
-                }
+	var tax = (glass + glueGlue + labourCost)*0.1;
+	var glassCost;
+
+	var num = (glass + glueGlue + labourCost + tax);
+	var outputCost = num.toFixed(2);
+
+			if(isNaN(length) || isNaN(width) || isNaN(height)) {
+				alert('Invalid length, width or height');
+                return;
+				}
 
     costObj.innerHTML = '';
-	costObj.innerHTML = "The cost is $" + (surfaceArea + (glueCost*.1) + labourCost + (tax/10));
+	costObj.innerHTML = "The cost is $" + outputCost;
 }
